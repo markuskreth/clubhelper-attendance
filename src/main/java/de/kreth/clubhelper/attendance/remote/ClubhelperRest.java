5356eb90b8df9c071f437a4df0a70de5c67d99e9
Markus Kreth <markus.kreth@web.de>
2020-09-06 18:22:27 +0200

    Data via rest service successful.

--- src/main/java/de/kreth/clubhelper/attendance/remote/ClubhelperRest.java
+++ src/main/java/de/kreth/clubhelper/attendance/remote/ClubhelperRest.java
@@ -3,23 +3,25 @@ package de.kreth.clubhelper.attendance.remote;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import de.kreth.clubhelper.attendance.data.Person;
 
 @Service
 public class ClubhelperRest {
 
-//    @Autowired
-//    private RestTemplate webClient;
+    @Autowired
+    private RestTemplate webClient;
 
     @Value("${resourceserver.api.url}")
     private String fooApiUrl;
 
     public List<Person> getPersons() {
 	String url = fooApiUrl + "/person";
-//	Person[] list = webClient.getForObject(url, Person[].class);
-	return Arrays.asList();
+	Person[] list = webClient.getForObject(url, Person[].class);
+	return Arrays.asList(list);
     }
 }
--- src/main/java/de/kreth/clubhelper/attendance/ui/MainView.java
+++ src/main/java/de/kreth/clubhelper/attendance/ui/MainView.java
@@ -1,20 +1,16 @@
 package de.kreth.clubhelper.attendance.ui;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import org.keycloak.KeycloakPrincipal;
-import org.keycloak.KeycloakSecurityContext;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import com.vaadin.flow.component.grid.Grid;
-import com.vaadin.flow.component.html.Anchor;
 import com.vaadin.flow.component.html.H1;
-import com.vaadin.flow.component.html.Span;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.data.provider.ListDataProvider;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.VaadinServlet;
 
 import de.kreth.clubhelper.attendance.data.Person;
 import de.kreth.clubhelper.attendance.remote.ClubhelperRest;
@@ -24,46 +20,32 @@ public class MainView extends VerticalLayout {
 
     private static final long serialVersionUID = 1L;
 
-    @Autowired
-    private ClubhelperRest rest;
-
     private Grid<Person> grid;
     private List<Person> personList;
 
     public MainView() {
 	createUi();
+	refreshData();
     }
 
     void refreshData() {
 	personList.clear();
-	personList.addAll(rest.getPersons());
+	personList.addAll(getRestService().getPersons());
     }
 
     private void createUi() {
 	add(new H1("Dies sind die Personen"));
 
-	Authentication authentication = SecurityContextHolder.getContext()
-		.getAuthentication();
-	if (!authentication.getPrincipal().equals("anonymousUser")) {
-	    KeycloakPrincipal<?> principal = (KeycloakPrincipal<?>) authentication.getPrincipal();
+	grid = new Grid<>(Person.class);
+	personList = new ArrayList<>();
+	ListDataProvider<Person> data = new ListDataProvider<>(personList);
+	grid.setDataProvider(data);
+	add(grid);
 
-	    KeycloakSecurityContext keycloakSecurityContext = principal.getKeycloakSecurityContext();
+    }
 
-	    String preferredUsername = keycloakSecurityContext.getIdToken().getPreferredUsername();
-	    Anchor logout = new Anchor(
-		    "http://localhost:9090/auth/realms/Demo/protocol/openid-connect/logout?redirect_uri=" +
-			    "http://localhost:9091/",
-		    "Logout");
-	    add(new HorizontalLayout(new Span(preferredUsername), logout));
-	} else {
-	    add(new Span("No Logged in User"));
-	}
-
-//	grid = new Grid<>(Person.class);
-//	personList = new ArrayList<>();
-//	ListDataProvider<Person> data = new ListDataProvider<>(personList);
-//	grid.setDataProvider(data);
-//	add(grid);
-
+    private ClubhelperRest getRestService() {
+	return WebApplicationContextUtils.getWebApplicationContext(VaadinServlet.getCurrent().getServletContext())
+		.getBean(ClubhelperRest.class);
     }
 }
