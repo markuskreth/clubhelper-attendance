4ce0bdde7e092319986daed322fc7a4356a26932
Markus Kreth <markus.kreth@web.de>
2020-10-31 21:02:51 +0100

    Hard coded locale Germany

--- src/main/java/de/kreth/clubhelper/attendance/ClubhelperAttendanceApplication.java
+++ src/main/java/de/kreth/clubhelper/attendance/ClubhelperAttendanceApplication.java
@@ -1,5 +1,7 @@
 package de.kreth.clubhelper.attendance;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
@@ -8,6 +10,7 @@ import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConf
 public class ClubhelperAttendanceApplication {
 
     public static void main(String[] args) {
+	Locale.setDefault(Locale.GERMANY);
 	SpringApplication.run(ClubhelperAttendanceApplication.class, args);
     }
 
