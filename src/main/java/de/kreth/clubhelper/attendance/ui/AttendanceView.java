c9faa51aae6fd4b150735d92efb85858c74bfe5e
Markus Kreth <markus.kreth@web.de>
2020-11-01 00:50:52 +0100

    added import

--- src/main/java/de/kreth/clubhelper/attendance/ui/AttendanceView.java
+++ src/main/java/de/kreth/clubhelper/attendance/ui/AttendanceView.java
@@ -2,6 +2,7 @@ package de.kreth.clubhelper.attendance.ui;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Locale;
 
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
