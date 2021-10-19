9da1356eaf32647d154924ce87490d7099524ddd
Markus Kreth <markus.kreth@web.de>
2021-10-19 15:29:10 +0200

    Bugfix - Id Vergleich

--- src/main/java/de/kreth/clubhelper/attendance/remote/BusinessImpl.java
+++ src/main/java/de/kreth/clubhelper/attendance/remote/BusinessImpl.java
@@ -26,95 +26,93 @@ import de.kreth.clubhelper.data.Person;
 @Service
 public class BusinessImpl implements Business {
 
-    protected Logger logger = LoggerFactory.getLogger(getClass());
+	protected Logger logger = LoggerFactory.getLogger(getClass());
 
-    @Autowired
-    private RestTemplate webClient;
+	@Autowired
+	private RestTemplate webClient;
 
-    @Value("${resourceserver.api.url}")
-    private String apiUrl;
+	@Value("${resourceserver.api.url}")
+	private String apiUrl;
 
-    @Override
-    public List<Person> getPersons() {
-	String url = apiUrl + "/person";
-	Person[] list = webClient.getForObject(url, Person[].class);
-	return Arrays.asList(list);
-    }
-
-    @Override
-    public PersonAttendance sendAttendance(PersonAttendance person, LocalDate attendanceDate, Boolean isAttendant) {
-
-	if (isAttendant != null && isAttendant.booleanValue()) {
-	    String url = apiUrl + "/attendance/for/" + person.getId();
-	    Attendance result = webClient.postForObject(url, attendanceDate, Attendance.class);
-	    if (result.getPerson().getId() != person.getId()) {
-		throw new IllegalStateException("Wrong person Id returned");
-	    }
-	    return PersonAttendance.createBy(result);
-	} else {
-	    String url = apiUrl + "/attendance/" + person.getId() + "/"
-		    + attendanceDate.format(DateTimeFormatter.ISO_DATE);
-	    webClient.delete(url);
-	    person.setOnDate(null);
-	    return person;
+	@Override
+	public List<Person> getPersons() {
+		String url = apiUrl + "/person";
+		Person[] list = webClient.getForObject(url, Person[].class);
+		return Arrays.asList(list);
 	}
-    }
 
-    @Override
-    public List<PersonAttendance> getAttendance(LocalDate date) {
+	@Override
+	public PersonAttendance sendAttendance(PersonAttendance person, LocalDate attendanceDate, Boolean isAttendant) {
 
-	String url = apiUrl + "/attendance/" + date.format(DateTimeFormatter.ISO_DATE);
-
-	try {
-	    List<Person> persons = new ArrayList<>(getPersons());
-	    Attendance[] body = webClient.getForObject(url, Attendance[].class);
-
-	    List<PersonAttendance> result = new ArrayList<>();
-	    for (Attendance attendance : body) {
-		persons.remove(attendance.getPerson());
-		result.add(PersonAttendance.createBy(attendance));
-	    }
-
-	    result.addAll(persons.stream()
-		    .map(PersonAttendance::createBy)
-		    .collect(Collectors.toList()));
-
-	    return result;
-	} catch (Exception e) {
-	    throw new RuntimeException(e);
+		if (isAttendant != null && isAttendant.booleanValue()) {
+			String url = apiUrl + "/attendance/for/" + person.getId();
+			Attendance result = webClient.postForObject(url, attendanceDate, Attendance.class);
+			if (!person.getId().equals(result.getPerson().getId())) {
+				throw new IllegalStateException("Wrong person Id returned");
+			}
+			return PersonAttendance.createBy(result);
+		} else {
+			String url = apiUrl + "/attendance/" + person.getId() + "/"
+					+ attendanceDate.format(DateTimeFormatter.ISO_DATE);
+			webClient.delete(url);
+			person.setOnDate(null);
+			return person;
+		}
 	}
-    }
 
-    @Override
-    public List<Contact> getContacts(Long personId) {
-	String url = apiUrl + "/contact/for/" + personId;
-	Contact[] contactArr = webClient.getForObject(url, Contact[].class);
+	@Override
+	public List<PersonAttendance> getAttendance(LocalDate date) {
 
-	return Arrays.asList(contactArr);
-    }
+		String url = apiUrl + "/attendance/" + date.format(DateTimeFormatter.ISO_DATE);
 
-    @Override
-    public Adress getAdress(Long personId) {
-	String url = apiUrl + "/adress/for/" + personId;
-	Adress[] arr = webClient.getForObject(url, Adress[].class);
+		try {
+			List<Person> persons = new ArrayList<>(getPersons());
+			Attendance[] body = webClient.getForObject(url, Attendance[].class);
 
-	return arr != null && arr.length > 0 ? arr[0] : null;
-    }
+			List<PersonAttendance> result = new ArrayList<>();
+			for (Attendance attendance : body) {
+				persons.remove(attendance.getPerson());
+				result.add(PersonAttendance.createBy(attendance));
+			}
 
-    @Override
-    public Authentication getCurrent() {
-	return SecurityContextHolder.getContext().getAuthentication();
-    }
+			result.addAll(persons.stream().map(PersonAttendance::createBy).collect(Collectors.toList()));
 
-    @Override
-    public List<GroupDef> getAllGroups() {
-	List<GroupDef> allGroups = new ArrayList<GroupDef>();
+			return result;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
 
-	String url = apiUrl + "/group";
-	GroupDef[] arr = webClient.getForObject(url, GroupDef[].class);
-	allGroups.addAll(Arrays.asList(arr));
-	return allGroups;
+	@Override
+	public List<Contact> getContacts(Long personId) {
+		String url = apiUrl + "/contact/for/" + personId;
+		Contact[] contactArr = webClient.getForObject(url, Contact[].class);
 
-    }
+		return Arrays.asList(contactArr);
+	}
+
+	@Override
+	public Adress getAdress(Long personId) {
+		String url = apiUrl + "/adress/for/" + personId;
+		Adress[] arr = webClient.getForObject(url, Adress[].class);
+
+		return arr != null && arr.length > 0 ? arr[0] : null;
+	}
+
+	@Override
+	public Authentication getCurrent() {
+		return SecurityContextHolder.getContext().getAuthentication();
+	}
+
+	@Override
+	public List<GroupDef> getAllGroups() {
+		List<GroupDef> allGroups = new ArrayList<GroupDef>();
+
+		String url = apiUrl + "/group";
+		GroupDef[] arr = webClient.getForObject(url, GroupDef[].class);
+		allGroups.addAll(Arrays.asList(arr));
+		return allGroups;
+
+	}
 
 }
